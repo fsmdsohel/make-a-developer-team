0083ec613986268d8c6389714977950f750f7f9d
fsmdsohel <fsmdshohelrana@gmail.com>
2021-09-29 05:49:04 +0600

    first commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,17 +4,17 @@ import "./Header.css";
 
 const Header = () => {
   return (
-      <div className="header">
-    <header>
-      <div className="header-content">
-        <h1>Make a developer team</h1>
-        <h2>budget 1M$</h2>
-      </div>
-      <div className="header-img">
-        <img src={introImg} alt="" />
-      </div>
-    </header>
-      </div>
+    <div className="header">
+      <header>
+        <div className="header-content">
+          <h1>Make a developer team</h1>
+          <h2>Budget 3 Million</h2>
+        </div>
+        <div className="header-img">
+          <img src={introImg} alt="" />
+        </div>
+      </header>
+    </div>
   );
 };
 
